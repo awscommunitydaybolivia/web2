81a0e3b025f966ba226385d066e614069df08bf2
Gonzalo Alfaro <gonzaloaf7@gmail.com>
2024-08-30 17:31:50 -0400

    edited speaker photo

--- src/data/speakersData.jsx
+++ src/data/speakersData.jsx
@@ -2,7 +2,7 @@ export const speakersData = [
   {
     name: "Ana Luiza Cunha",
     jobTitle: "Senior Developer Advocate en AWS ",
-    image: "https://aws-cd-bo-web.s3.amazonaws.com/speakers-aws-cd-bo-2024/aws-cd-bo-speaker01.webp",
+    image: "https://aws-cd-bo-web.s3.amazonaws.com/speakers-aws-cd-bo-2024/aws-cd-bo-speaker01a.webp",
     description: "Description for Speaker 1",
     linkedin: "https://www.linkedin.com/in/analuizacunha/",
     twitter: "https://x.com/mcunhaana",
