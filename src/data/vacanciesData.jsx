788f06f0b6cd5d4d9947aa53f097a8a73970fa01
Gonzalo Alfaro <gonzaloaf7@gmail.com>
2024-09-27 13:51:20 -0400

    2 positions were added to the vacancies

--- src/data/vacanciesData.jsx
+++ src/data/vacanciesData.jsx
@@ -59,6 +59,30 @@ export const vacanciesData = [
     workType: "Híbrido",
     url: "https://forms.office.com/r/4cJrt5fM2c",
   },
+  {
+    companyImg: "https://aws-cd-bo-web.s3.amazonaws.com/aws-cd-bo-sponsors-files/sponsors-logos/logo-cloudreams.png",
+    title: "Sr. Mobile Developer",
+    seniority: "Senior",
+    english: "Advanced",
+    yearsOfExperience: "5+ years of experience in mobile app development.",
+    skills: ["React Native, Swift (iOS), Kotlin (Android), Android Auto, CarPlay, Android Automotive"],
+    competences: ["Mobile app development (iOS & Android), Integration with in-vehicle systems, Strong problem-solving and debugging, Collaboration with cross-functional teams"],
+    region: "Latam",
+    workType: "100% work from home",
+    url: "https://cloudreams.net/job-application/sr-mobile-developer/",
+  },
+  {
+    companyImg: "https://aws-cd-bo-web.s3.amazonaws.com/aws-cd-bo-sponsors-files/sponsors-logos/logo-cloudreams.png",
+    title: "Sr. Java Developer",
+    seniority: "Senior",
+    english: "Advanced",
+    yearsOfExperience: "5+ years",
+    skills: ["Java, Spring Boot, Microservices architecture, REST API design, SOLID principles, Relational databases (MySQL, PostgreSQL)"],
+    competences: ["Systems architecture, Full software development lifecycle, Performance optimization, Code review and mentoring, Collaboration with cross-functional teams"],
+    region: "Latam",
+    workType: "100% work from home",
+    url: "https://cloudreams.net/job-application/sr-java-developer/",
+  },
 ];
 
 export default vacanciesData;
