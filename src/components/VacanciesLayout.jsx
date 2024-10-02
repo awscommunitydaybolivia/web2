095f6f25318f2135f081714e24e5fb2ee03a84f2
Gonzalo Alfaro <gonzaloaf7@gmail.com>
2024-10-01 20:07:22 -0400

    updated vacanciesLayout img

--- src/components/VacanciesLayout.jsx
+++ src/components/VacanciesLayout.jsx
@@ -33,10 +33,10 @@ const VacanciesLayout = ({ className = "" }) => {
           </div>
         </div>
         <img
-          className="h-[640px] flex-1 relative max-w-full overflow-hidden object-cover min-w-[400px] mq800:min-w-full"
+          className="h-max flex-1 relative max-w-full overflow-hidden object-cover min-w-[400px] mq800:min-w-full"
           loading="lazy"
           alt=""
-          src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://aws-cd-bo-web.s3.amazonaws.com/aws-cd-bo-web-imgs/vacancies-aws-cd-bo.webp"
         />
       </div>
     </section>
